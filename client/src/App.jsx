75595d45b76667371619dbfee839ffa985524237
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-11 22:16:13 +0530

    connected state added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,31 @@
 import "./App.scss";
 import { useState } from "react";
 import UsernameForm from "./components/UsernameForm";
+import ChatApp from "./components/ChatApp";
 import io from "socket.io-client";
 import { useEffect } from "react";
 
 function App() {
     const socket = io("http://localhost:8080", { autoConnect: false });
+    const [connected, setConnected] = useState(socket.connected);
     const [username, setUsername] = useState("");
 
     useEffect(() => {
         socket.onAny((event, ...args) => {
             console.log(event, ...args);
         });
+        socket.on("connect", () => {
+            setConnected(true);
+        });
+
+        socket.on("disconnect", () => {
+            setConnected(false);
+        });
+
+        return () => {
+            socket.off("connect");
+            socket.off("disconnect");
+        };
     });
 
     const handleOnSubmit = (event) => {
@@ -21,13 +35,20 @@ function App() {
         socket.connect();
     };
 
+    if (!connected) {
+        return (
+            <>
+                <UsernameForm
+                    username={username}
+                    setUsername={setUsername}
+                    handleOnSubmit={handleOnSubmit}
+                />
+            </>
+        );
+    }
     return (
         <>
-            <UsernameForm
-                username={username}
-                setUsername={setUsername}
-                handleOnSubmit={handleOnSubmit}
-            />
+            <ChatApp socket={socket}></ChatApp>
         </>
     );
 }
