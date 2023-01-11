6e551dbc82f64d2b81ad028ac523b601f2371eb5
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-11 22:28:16 +0530

    socket.io connection moved in the component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,40 +2,18 @@ import "./App.scss";
 import { useState } from "react";
 import UsernameForm from "./components/UsernameForm";
 import ChatApp from "./components/ChatApp";
-import io from "socket.io-client";
-import { useEffect } from "react";
 
 function App() {
-    const socket = io("http://localhost:8080", { autoConnect: false });
-    const [connected, setConnected] = useState(socket.connected);
+    const [formFilled, setFormFilled] = useState(false);
     const [username, setUsername] = useState("");
-
-    useEffect(() => {
-        socket.onAny((event, ...args) => {
-            console.log(event, ...args);
-        });
-        socket.on("connect", () => {
-            setConnected(true);
-        });
-
-        socket.on("disconnect", () => {
-            setConnected(false);
-        });
-
-        return () => {
-            socket.off("connect");
-            socket.off("disconnect");
-        };
-    });
 
     const handleOnSubmit = (event) => {
         event.preventDefault();
         console.log(event);
-        socket.auth = { username: event.target.elements.username.value };
-        socket.connect();
+        setFormFilled(true);
     };
 
-    if (!connected) {
+    if (!formFilled) {
         return (
             <>
                 <UsernameForm
@@ -48,7 +26,7 @@ function App() {
     }
     return (
         <>
-            <ChatApp socket={socket}></ChatApp>
+            <ChatApp username={username} />
         </>
     );
 }
--- client/src/components/ChatApp.jsx
+++ client/src/components/ChatApp.jsx
@@ -1,12 +1,43 @@
 import React from "react";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
 
-const ChatApp = ({ socket }) => {
-    console.log(socket);
+const ChatApp = ({ username }) => {
+    const socket = io("http://localhost:8080", {
+        auth: {
+            username,
+        },
+        autoConnect: false,
+    });
+    const [connected, setConnected] = useState(socket.connected);
+
+    useEffect(() => {
+        socket.onAny((event, ...args) => {
+            console.log(event, ...args);
+        });
+        socket.on("connect", () => {
+            setConnected(true);
+        });
+
+        socket.on("disconnect", () => {
+            setConnected(false);
+        });
+
+        return () => {
+            socket.off("connect");
+            socket.off("disconnect");
+        };
+    });
+
+    useEffect(() => {
+        socket.connect();
+    }, []);
 
     return (
         <>
             <h1>Chatting App</h1>
-            <p>For the user {socket.username}</p>
+            <p>For the user {username}</p>
+            <h1>{connected ? "Connected" : "Not Connected"}</h1>
         </>
     );
 };
