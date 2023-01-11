3f6abdf0a7a5e9d8dd70aa2578382612f3833bac
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-11 20:25:53 +0530

    use morgan for logs

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const morgan = require("morgan");
 
 // middlewares.
+app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 
 module.exports = app;
