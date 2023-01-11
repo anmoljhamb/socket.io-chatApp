0efd66a4214b563e53982fe751eb183d8e22e90d
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-11 21:09:28 +0530

    Added cors as a middleware

--- server/app.js
+++ server/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const morgan = require("morgan");
+const cors = require("cors");
 
 // middlewares.
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 
